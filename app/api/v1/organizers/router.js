031fe908b44ced061a5f01eb975f2f8a39459b1c
gillpann <cpundong@gmail.com>
2025-05-06 11:22:24 +0700

    fix: organizer router and categories

--- app/api/v1/organizers/router.js
+++ app/api/v1/organizers/router.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const router = express.Router();
-const { createCMSOrganizer, createCMSUser } = require("./controller");
+const {
+  createCMSOrganizer,
+  createCMSUser,
+  getCMSUsers,
+} = require("./controller");
 
 const {
   authenticateUser,
-//   authorizeRoles,
+  authorizeRoles,
 } = require("../../../middleware/auth");
 
-router.post("/organizers", createCMSOrganizer);
-router.post("/users", authenticateUser, createCMSUser);
+router.post(
+  "/organizers",
+  authenticateUser,
+  authorizeRoles("owner"),
+  createCMSOrganizer
+);
+router.post(
+  "/users",
+  authenticateUser,
+  authorizeRoles("organizer"),
+  createCMSUser
+);
+
+router.get(
+  "/users",
+  authenticateUser,
+  authorizeRoles("owner"),
+  getCMSUsers,
+);
 
 module.exports = router;
