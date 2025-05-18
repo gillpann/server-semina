708da8cd5d9da1869f4333e82ad2f98a06e5c24a
gillpann <cpundong@gmail.com>
2025-05-18 16:11:37 +0700

    fix: router and JWT

--- app/config.js
+++ app/config.js
@@ -2,7 +2,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    urlDb: process.env.URL_MONGODB_DEV, 
-    jwtExpiration: '24h', // expiration time for JWT
-    jwtSecret: 'jwtSecret', // secret key for JWT
+    urlDb: process.env.URL_MONGODB_DEV, // your MongoDB connection string
+    jwtExpiration: process.env.JWT_EXPIRATION, // expiration time for JWT
+    jwtSecret: process.env.JWT_SECRET, // your secret key
+    gmail: process.env.GMAIL, // your gmail address
+    password: process.env.PASSWORD, // your gmail password
 };
