02dba846fd89ad6b68f6d341549cb1d838fc1176
gillpann <cpundong@gmail.com>
2025-05-18 20:50:37 +0700

    feat: checkout API and set payments

--- app/services/mail/index.js
+++ app/services/mail/index.js
@@ -30,4 +30,47 @@ const otpMail = async (email, data) => {
   }
 };
 
-module.exports = { otpMail };
+const orderMail = async (email, data) => {
+  try {
+    data.orderItems = data.orderItems.map((item) => {
+      return {
+        ...item,
+        subtotal: item.ticketCategories.price * item.sumTicket,
+      };
+    });
+
+    data.date = new Date(data.date).toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+
+    data.historyEvent.date = new Date(
+      data.historyEvent.date
+    ).toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    let template = fs.readFileSync("app/views/email/orders.html", "utf8");
+
+    let message = {
+      from: gmail,
+      to: email,
+      subject: `Semina - Invoice for ${data.historyEvent.title}`,
+      html: Mustache.render(template, data),
+    };
+
+    return await transporter.sendMail(message);
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+
+module.exports = { otpMail, orderMail };
