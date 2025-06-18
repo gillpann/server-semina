46a967834cefa7d3035783ff66a3ff6c1b47fdcf
gillpann <cpundong@gmail.com>
2025-06-18 19:13:32 +0700

    feat: refresh token

--- app/config.js
+++ app/config.js
@@ -2,9 +2,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    urlDb: process.env.URL_MONGODB_DEV, // your MongoDB connection string
-    jwtExpiration: process.env.JWT_EXPIRATION, // expiration time for JWT
-    jwtSecret: process.env.JWT_SECRET, // your secret key
-    gmail: process.env.GMAIL, // your gmail address
-    password: process.env.PASSWORD, // your gmail password
+  urlDb: process.env.URL_MONGODB_DEV, // your MongoDB connection string
+  jwtExpiration: process.env.JWT_EXPIRATION, // expiration time for JWT
+  jwtSecret: process.env.JWT_SECRET_KEY, // your secret key
+  jwtRefreshTokenSecret: process.env.JWT_SECRET_KEY_REFRESH_TOKEN, // your secret key for refresh token
+  jwtRefreshTokenExpiration: process.env.JWT_EXPIRATION_REFRESH_TOKEN, // expiration time for refresh token
+  gmail: process.env.GMAIL, // your gmail address
+  password: process.env.PASSWORD, // your gmail password
 };
